ca86c30c8283f4c3701a0f70a359ac19bebb7b15
Helena <helena@klarr.se>
2023-08-29 17:33:35 +0200

    starter code for array methods exercise

---
+++ script.js
@@ -0,0 +1,5 @@
+// funktion för att formatera nummer som pengar
+// https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
